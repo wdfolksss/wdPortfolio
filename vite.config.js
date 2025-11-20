4e000732ec6bce9d9d8be288e0abfe9ae704d6e5
Warda Amalia <amaliawarda411@gmail.com>
2025-11-20 13:09:34 +0700

    sedikit update wkwk

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wdPortofolio/', // ganti sesuai nama repo GitHub kamu
+  base: '/wdPortfolio/', // ganti sesuai nama repo GitHub kamu
 })
